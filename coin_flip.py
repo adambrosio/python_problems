34ea51c45f14419c68b8f7551c65f8ad7feab6ff
Andrew D'Ambrosio <adambrosio20@gmail.com>
2021-04-13 10:43:21 -0400

    flip function created

--- coin_flip.py
+++ coin_flip.py
@@ -6,7 +6,26 @@ choices = ('H','T')
 heads_count = 0
 tails_count = 0
 
-print('Welcome to Coin Flip!')
+def flip():
+    while True:
+        player_choice = input("Choose Heads or Tails by entering 'H' or 'T': ")
+        
+        toss = random.choice(choices)
+
+        if player_choice[0].upper() == 'H' and toss == 'H':
+            heads_count += 1
+            print('You won!')
+        elif player_choice[0].upper() == 'T' and toss == 'T':
+            tails_count += 1
+            print('You won!')
+        elif player_choice[0].upper() == 'H' and toss == 'T':
+            print('You lost!')
+        elif player_choice[0].upper() == 'T' and toss == 'H':
+            print('You lost!')
+        else:
+            print('Invalid input. Please try again.')
+            continue
+        break
 
 while playing:
-    pass
+    print('Welcome to Coin Flip!')
